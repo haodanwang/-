fccb3bdae312238f1b6c42753d4fafad51f15666
WHD <939565638@qq.com>
2018-11-21 07:23:31 +0800

    更新练习题
    Signed-off-by: WHD <939565638@qq.com>

--- day01/day11.py
+++ day01/day11.py
@@ -63,9 +63,15 @@ L=list(range(100))
 #练习
 #利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
 def trim(s):
-    while s[:1]=='':
-        s=s[1:]
-        while str[-1:]=='':
-            s=s[:-1]
+    L=len(s)
+    while L>0:
+        L=L-1
+        if s[:1]==' ':
+            s=s[1:]
+        elif s[-1:]==' ':
+            s=s[:-2]
+        else:
+            break
     return s
-print(trim('Hekko world'))
+print(trim('    Hello  '))
+print('Hello')
