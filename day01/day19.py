690af543cae6710fe8c1b5f5321c8fb401cdce1b
WHD <939565638@qq.com>
2019-12-09 21:11:49 +0800

    1

--- day01/day19.py
+++ day01/day19.py
@@ -1,13 +1,16 @@
-#模块
-import  sys
-def test():
-    args=sys.argv
-    if len(args)==1:
-        print('Hello world')
-    elif len(args)==2:
-        print('Hello %s!'% args[1])
-    else:
-        print('Too many argument')
+# #模块
+# # import  sys
+# # def test():
+# #     args=sys.argv
+# #     if len(args)==1:
+# #         print('Hello world')
+# #     elif len(args)==2:
+# #         print('Hello %s!'% args[1])
+# #     else:
+# #         print('Too many argument')
+# #
+# # if __name__=='__main__':
+# #     test()
 
-if __name__=='__main__':
-    test()
+
+
